181196d6268b2e631ef237f6309a314f16c3d505
Pat <patrickross216@gmail.com>
2025-05-29 17:32:53 +1000

    Adds manager portal page to easily access admin tools

--- Bean-Scene-Reservation/Controllers/HomeController.cs
+++ Bean-Scene-Reservation/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Bean_Scene_Reservation.Controllers
             return View();
         }
 
+        public IActionResult ManagerPortal()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
