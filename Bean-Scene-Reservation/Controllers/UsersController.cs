4098a44afc474be04b9f98c84d267a9f1ebabfd9
Pat <patrickross216@gmail.com>
2025-06-13 09:58:38 +1000

    Updates user controller to have a functioning index and details view

--- Bean-Scene-Reservation/Controllers/UsersController.cs
+++ Bean-Scene-Reservation/Controllers/UsersController.cs
@@ -1,27 +1,74 @@
 ﻿using Bean_Scene_Reservation.Data;
+using Bean_Scene_Reservation.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 
 namespace Bean_Scene_Reservation.Controllers
 {
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public UsersController(ApplicationDbContext context)
+        public UsersController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         // GET: Users
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Users;
-            return View(await applicationDbContext.ToListAsync());
+            var users = await _userManager.Users.ToListAsync();
+            var usersWithRoles = new List<UserWithRolesDto>();
+
+            foreach (var user in users) {
+                var roles = await _userManager.GetRolesAsync(user);
+                usersWithRoles.Add(new UserWithRolesDto
+                {
+                    Id = user.Id,
+                    FullName = user.FullName,
+                    Email = user.Email,
+                    Phone = user.PhoneNumber,
+                    Roles = roles.ToList()
+                });
+            }
+
+            return View(usersWithRoles);
         }
 
         // GET: Users/Details/1ac0095d-c987-4dfc-a387-0ad330077fe1
+        [HttpGet("Users/Details/{id}")]
+        public async Task<IActionResult> Details (string? id)
+        {
+            if (id == null) {
+                return NotFound();
+            }
 
+            var user = await _userManager.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (user == null) {
+                return NotFound();
+            }
+
+            var roles = await _userManager.GetRolesAsync(user);
+
+            var userWithRoles = new DetailedUserDto
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                Email = user.Email,
+                Phone = user.PhoneNumber,
+                Roles = roles.ToList(),
+                EmailConfirmed = user.EmailConfirmed,
+                PhoneConfirmed = user.PhoneNumberConfirmed,
+                TwoFactorEnabled = user.TwoFactorEnabled
+            };
+
+            return View(userWithRoles);
+        }
 
         // GET: Users/Create
 
@@ -35,6 +82,32 @@ namespace Bean_Scene_Reservation.Controllers
 
         // POST: Users/Delete/1ac0095d-c987-4dfc-a387-0ad330077fe1
 
+
+        public class UserWithRolesDto
+        {
+            public string Id { get; set; }
+            [DisplayName("Full Name")]
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public List<string> Roles { get; set; }
+        }
+
+        public class DetailedUserDto
+        {
+            public string Id { get; set; }
+            [DisplayName("Full Name")]
+            public string FullName { get; set; }
+            public string Email { get; set; }
+            public string Phone { get; set; }
+            public List<string> Roles { get; set; }
+            [DisplayName("Email Confirmed")]
+            public bool EmailConfirmed { get; set; }
+            [DisplayName("Phone Confirmed")]
+            public bool PhoneConfirmed { get; set; }
+            [DisplayName("Two Factor Enabled")]
+            public bool TwoFactorEnabled { get; set; }
+        }
 
     }
 }
