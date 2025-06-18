a112cfe74479b0afc52a17e23cb44ca966f15a7f
Pat <jack.brown3@studytafensw.edu.au>
2025-06-18 12:34:33 +1000

    Updates the connection string to be connected to Azure

--- Bean-Scene-Reservation/Program.cs
+++ Bean-Scene-Reservation/Program.cs
@@ -18,7 +18,7 @@ namespace Bean_Scene_Reservation
             // Add services to the container.
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("DatabaseConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
